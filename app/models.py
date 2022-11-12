dd93dde250f48bfae6dce5b4edf0e31e26001630
Cristobal <contacto@gmail.com>
2022-11-12 20:08:08 -0300

    final

--- app/models.py
+++ app/models.py
@@ -1,12 +1,27 @@
 from django.db import models
+from django.core.validators import validate_email
 
 # Create your models here.
 
+Sexo = (
+    ("F", "Femenino"),
+    ("M", "Masculino"),
+)
+Estado = (
+    ("VIG", "Vigente"),
+    ("SUS", "Suspendido"),
+    ("RET", "Retirado"),
+)
 class Socio(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=20)
     fechaIncorporacion = models.DateField()
     anoNacimiento = models.DateField()
     telefono = models.IntegerField()
-    correo = models.EmailField()
-    sexo = models.CharField(max_length=20)
-    estado = models.CharField(max_length=20)
+    correo = models.EmailField(validators=[validate_email])
+    sexo = models.CharField(max_length=20, choices=Sexo, default="F")
+    # estado = models.CharField(max_length=20)
+    estado = models.CharField(
+        max_length = 20,
+        choices = Estado,
+        default = 'VIG'
+    )
